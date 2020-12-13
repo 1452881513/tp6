6b27b7f048d2a886ff1b4efbd260cdbc3142aa91
starry <504750567@qq.com>
2020-12-13 17:46:07 +0800

    第一次提交（ï多英多应用苼

---
+++ app/admin/controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Index.php
+ * 2020/12/13 17:17
+ */
+
+namespace app\admin\controller;
+
+
+class Index
+{
+    public function index()
+    {
+        return 'admin1123123';
+    }
+
+    public function edis()
+    {
+        return '123123';
+    }
+}
